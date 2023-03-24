a8d403010a638b3d55ce57be1eadcdf39623e071
ghtali <ght.ali@hotmail.com>
2023-03-24 20:43:07 +0100

    init labtests model

---
+++ models/lab_tests.py
@@ -0,0 +1,10 @@
+
+class LabTest:
+    def __init__(self, test_id, appointment_id, test_name, date_created,
+                 date_completed, result):
+        self.test_id = test_id,
+        self.appointment_id = appointment_id,
+        self.test_name = test_name,
+        self.date_created = date_created,
+        self.date_completed = date_completed,
+        self.result = result
