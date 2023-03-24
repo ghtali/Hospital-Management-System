1f0419389f7ba3346eb65253db5c8a0b318a28dd
ghtali <ght.ali@hotmail.com>
2023-03-24 20:43:38 +0100

    init patients model

---
+++ models/patients.py
@@ -0,0 +1,12 @@
+
+class Patient:
+    def __init__(self, patient_id, first_name, last_name, date_of_birth,
+                 gender, address, phone_number, email):
+        self.patient_id = patient_id,
+        self.first_name = first_name,
+        self.last_name = last_name,
+        self.date_of_birth = date_of_birth,
+        self.gender = gender,
+        self.address = address,
+        self.phone_number = phone_number,
+        self.email = email
