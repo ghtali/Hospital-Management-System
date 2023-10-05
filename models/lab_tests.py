483cf0150ce4774aae3307d6353aa792c9e37104
ght-ali <ght.ali@hotmail.com>
2023-10-06 01:04:28 +0200

    delete request for lab test endpoint

--- models/lab_tests.py
+++ models/lab_tests.py
@@ -22,17 +22,24 @@ def get_all_lab_tests():
 
 @lab_tests_api_bp.route('/lab_tests/<int:lab_test_id>', methods=['GET'])
 def get_lab_test(lab_test_id):
-    # TODO: Implement get_lab_test endpoint
-    pass
+    lab_test = lab_tests_service.get_lab_test_by_id(lab_test_id)
+    if lab_test:
+        return jsonify(lab_test)
+    return jsonify({'error': 'Lab Test not found'}), 404
 
 
 @lab_tests_api_bp.route('/lab_tests/<int:lab_test_id>', methods=['PUT'])
 def update_lab_test(lab_test_id):
-    # TODO: Implement update_lab_test endpoint
-    pass
+    data = request.json
+    updated_lab_test = lab_tests_service.update_lab_test(lab_test_id, data)
+    if updated_lab_test:
+        return jsonify(updated_lab_test)
+    return jsonify({'error': 'Lab Test not found or update failed'}), 404
 
 
 @lab_tests_api_bp.route('/lab_tests/<int:lab_test_id>', methods=['DELETE'])
 def delete_lab_test(lab_test_id):
-    # TODO: Implement delete_lab_test endpoint
-    pass
+    result = lab_tests_service.delete_lab_test(lab_test_id)
+    if result:
+        return jsonify({'message': 'Lab Test deleted successfully'}), 200
+    return jsonify({'error': 'Lab Test not found or deletion failed'}), 404
